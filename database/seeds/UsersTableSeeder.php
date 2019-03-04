9bfaf321a050dacf839db88fd3446fbfe2564842
Cloudnextgeneration <info@cloudnextbd.com>
2019-03-04 17:11:03 +0600

    Blog App

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'role_id'=>1,
+            'name'=>'Akash Mallik',
+            'username'=>'admin',
+            'email'=>'toakashmallik@gmail.com',
+            'password'=>bcrypt('admin@123')
+        ]);
+        DB::table('users')->insert([
+            'role_id'=>2,
+            'name'=>'Mr. Author',
+            'username'=>'author',
+            'email'=>'hey.knock.me@gmail.com',
+            'password'=>bcrypt('author@123')
+        ]);
+    }
+}
